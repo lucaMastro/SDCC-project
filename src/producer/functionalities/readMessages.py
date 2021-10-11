199c452f8c43610ef544640f8e2b00c269c6b5ae
Luca <luca.mastrobattista@students.uniroma2.eu>
2021-10-11 16:14:44 +0200

    changed logic in read-message CLI

--- src/producer/functionalities/readMessages.py
+++ src/producer/functionalities/readMessages.py
@@ -5,22 +5,77 @@ import signal
 import sys 
 
 import functionalities.deleteMessagesAndMarkAsRead as delAndMark
+import functionalities.Message as mess 
 
-readMessages = []
-toDeleteMessages = []
 messagesList = []
-payload = None
+
+# class for manage use case: it's a subclass of Message
+class ToDisplayMessage(mess.Message):
+    deleted = False
+    read = False 
+    index = None
+    key = ''
+    isNew = None 
+
+    def __init__(self, msgString, position, key, new):
+        super().__init__(msgString) # filling Message's field
+        self.index = position 
+        self.key = key 
+        self.isNew = new 
+
+
+class MessageList():
+    toDisplayMessageList = []
+    numOfDeleted = 0
+
+    def __init__(self):
+        self.toDisplayMessageList = list()
+        self.numOfDeleted = 0
+
+    def __len__(self):
+        return len(self.toDisplayMessageList) - self.numOfDeleted
+
+    def append(self, item):
+        self.toDisplayMessageList.append(item)
+
+    def __getitem__(self, i):
+        if i > len(self):
+            raise IndexError('list index out of range')
+        else:
+            for item in self.toDisplayMessageList:
+                if item.index == i and not item.deleted:
+                    return item
+
+    def delete(self, i):
+        if i >= len(self):
+            raise IndexError('list index out of range')
+        else:
+            item = self[i]
+            item.deleted = True
+            self.numOfDeleted += 1
+            for i in range(i + 1, len(self.toDisplayMessageList)):
+                self.toDisplayMessageList[i].index -= 1
+
+    def getRead(self):
+        l = []
+        for i in self.toDisplayMessageList:
+            if i.read:
+                l.append(i)
+
+    def __str__(self):
+        s = '['
+        for i in range(len(self) - 1):
+            item = self[i]
+            s += str(item) + ', '
+        s += str(self[len(self) - 1]) + ']'
+        return s
+
 
 def getMessages(username, all_ = True, graphic=False):
     
-    global readMessages
-    global toDeleteMessages
     global messagesList 
-    global payload
 
-    messagesList = []
-    readMessages = []
-    toDeleteMessages = []
+    messagesList = MessageList() 
 
     client = boto3.client('lambda')
     input_params = {}
@@ -32,65 +87,31 @@ def getMessages(username, all_ = True, graphic=False):
             Payload=json.dumps(input_params), )
 
     strPayload = response['Payload'].read().decode('utf-8')
-
     payload = ast.literal_eval(strPayload)
 
     # filling messagesList with the bodies
-    for key in payload.keys():
-        messagesList.append(payload[key]['message'])
+    i = 0
+    for keyIndex in payload.keys():
+        msgStr = payload[keyIndex]['message']
+        
+        if payload[keyIndex]['new'] == 'True':
+            isNew = True
+        else:
+            isNew = False 
+
+        key = payload[keyIndex]['key']
+
+        messagesList.append(ToDisplayMessage(msgStr, i, key, isNew))
+        i += 1
+        #messagesList.append(payload[key]['message'])
 
     if not graphic:
         showMessages(username, all_)
-        prepareAndInvokeDelete()
         
-def signalHandler(*args):
-    print('\nPlease wait few seconds..\n')
-    prepareAndInvokeDelete()
-    sys.exit()
-
-def prepareAndInvokeDelete():
-    global readMessages
-    global toDeleteMessages 
-    global payload 
-
-    # now readMessages and toDeleteMessages lists are filled. use them to
-    # create dict that will be used as input of lambda for deleting and marking
-    # as read:
-
-    # schedule messages to be mark as read. I'm going to remove indexes
-    # relatives to messages that are already read:
-    for i in range(len(payload)):
-        if payload[str(i)]['new'] == 'False' and \
-                i in readMessages:
-                    readMessages.remove(i)
-    # then, i don't want to mark as read a message that will be deleted:
-    for i in toDeleteMessages:
-        if i in readMessages:
-            readMessages.remove(i)
-
-    # following dict will keep pairs like <key-name-in-s3>:<operation>
-    # where operation is 'del' or 'mark'
-    # if both lists are empty, no operation is needed:
-    if readMessages != [] or toDeleteMessages != []:
-        inputDict = {}
-        for i in readMessages:
-            inputDict[payload[str(i)]['key']] = 'mark'
-
-        for i in toDeleteMessages:
-            inputDict[payload[str(i)]['key']] = 'del'
-        
-        delAndMark.manageDelAndMark(inputDict)
-
-    # reinitializing payload
-    payload = None
-
 
 # CLI read-messages support
 def showMessages(username, all_):
     global messagesList
-    global readMessages
-    global toDeleteMessages 
-    global payload 
 
     # command line changing handler for ctrl +c: in this way, even if a ctrl+c
     # is execute, the changes will be sent to aws
@@ -99,8 +120,12 @@ def showMessages(username, all_):
     # changing handler:
     signal.signal(signal.SIGINT, signalHandler)
     
+    s = "You don't have any " 
     if (len(messagesList) == 0):
-        print("You don't have any message to display.")
+        if not all_:
+            s += 'new '
+        s += "message to display."
+        print(s)
         return
 
     if (all_):
@@ -112,27 +137,26 @@ def showMessages(username, all_):
     while current < len(messagesList):
         print('-' * 60)
         print('index: {}\tNew: {}\n'.format(current + 1,
-            payload[str(current)]['new']))
+            messagesList[current].isNew))
         print('.' * 30)
         print(messagesList[current])
         print('.' * 30)
 
-        if current not in readMessages:
-            readMessages.append(current)
+        messagesList[current].read = True 
 
         print('Give me a command:')
         print('c = show next, b = break, d = delete this message, j <n>' +
         ' = show n-th message\n')
-        cmd = input('user: {} >> '.format(username))
+        cmd = input('user: {} - read >> '.format(username))
 
         if cmd == 'c':
             current += 1
         elif cmd == 'b':
             break
         elif cmd == 'd':
-            if current not in toDeleteMessages:
-                toDeleteMessages.append(current)
-            current += 1
+            messagesList.delete(current)
+            # not increment: the current-th message is changed
+            #current += 1
         else:
             # expecting a j n command
             params = cmd.split(' ')
@@ -156,12 +180,43 @@ def showMessages(username, all_):
             # n-th mess is in n-1 position
             current = n - 1
 
+    # all messages have ben read
     if current == len(messagesList):
         print("You don't have other messages.")
     
+    # make changes on aws
+    prepareAndInvokeDelete()
     # restoring previous handler:
     signal.signal(signal.SIGINT, default_handler)
 
+def signalHandler(*args):
+    print('\nPlease wait few seconds..\n')
+    prepareAndInvokeDelete()
+    sys.exit()
+
+def prepareAndInvokeDelete():
+    readMessages = []
+    toDeleteMessages = [] 
+
+    # scanning the complete list of messages:
+    for i in messagesList.toDisplayMessageList: 
+        # mark as read only if not deleted
+        if i.deleted:
+            toDeleteMessages.append(i.key)
+        else:
+            if i.read:
+                readMessages.append(i.key)
+
+    # if both lists are empty, no operation is needed:
+    if readMessages != [] or toDeleteMessages != []:
+        inputDict = {}
+        for key in readMessages:
+            inputDict[key] = 'mark'
+
+        for key in toDeleteMessages:
+            inputDict[key] = 'del'
+        
+        delAndMark.manageDelAndMark(inputDict)
 
 if __name__ == '__main__':
     #payload = '["From: luca\nTo: luca\nObject: test2\nBody: provaprova\n\n", "From: luca\nTo: luca\nObject: asd\nBody: prova\n\n"]'
